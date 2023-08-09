76682579e22b664ba4b2c0d08213c960bf71e1d1
Abbey Moss <abbeymossmusic@gmail.com>
2023-08-09 15:58:09 -0500

    workingthrucommmentingout

---
+++ src/views/categoriesform.js
@@ -0,0 +1,30 @@
+// import { useEffect, useState } from "react"
+// import { useNavigate } from "react-router-dom";
+// import { newCategory } from "../managers/Categories";
+
+// export const newCategoryForm = () => {
+    
+//     const [newCategoryArray, changeNewCategoryArray] = useState([])
+//     const  [category, updateCategory] = useState({
+//         "label": ""
+//     })
+
+//     useEffect(() => {
+
+//     })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
