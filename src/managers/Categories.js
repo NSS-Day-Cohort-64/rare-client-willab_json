f7b9dbcad290173d192864a8e36759c54ec2c164
Abbey Moss <abbeymossmusic@gmail.com>
2023-08-09 15:48:05 -0500

    attempting aside

--- src/managers/Categories.js
+++ src/managers/Categories.js
@@ -2,3 +2,16 @@ export const listCategory = (category) => {
     return fetch("http://localhost:8088/categories", {
     }).then(res => res.json())
 }
+
+export const newCategory = (category) => {
+    return fetch(`http://localhost:8088/categories`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(category)
+    })
+        .then(response => response.json())
+        .then(() => {
+        })
+    }
