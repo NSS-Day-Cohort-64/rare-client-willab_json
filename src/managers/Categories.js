c710ea3590135412bcef0f2683276a1303e8323f
Abbey Moss <abbeymossmusic@gmail.com>
2023-08-10 10:26:11 -0500

    worked with will to unify client

--- src/managers/Categories.js
+++ src/managers/Categories.js
@@ -3,15 +3,3 @@ export const listCategory = (category) => {
     }).then(res => res.json())
 }
 
-export const newCategory = (category) => {
-    return fetch(`http://localhost:8088/categories`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(category)
-    })
-        .then(response => response.json())
-        .then(() => {
-        })
-    }
--- src/views/categories.js
+++ src/views/categories.js
@@ -1,10 +1,14 @@
+import { listCategory } from "../managers/Categories";
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom";
-import { listCategory } from "../managers/Categories";
+
 
 export const Categories = () => {
+    const [newCategories, setNewCategories] = useState([])
+    const [label, setLabel] = useState('')
+    const [count, setCount] = useState([0])
     const [categories, setCategories] = useState([])
-    const [error, setError] = useState(null)   
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         listCategory() // Assuming this function returns a promise
@@ -16,14 +20,52 @@ export const Categories = () => {
             });
     },
         [])
+    
+        useEffect(() => {
+        console.log('rerendering')
+    }, [count])
 
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        // Process the label (e.g., send it to an API, update state, etc.)
+        const categoryToBeSentToServer = {
+            label: label
+        }
+        listCategory() // Assuming this function returns a promise
+            .then(data => {
+                setCategories(data);
+            })
+            .catch(err => {
+                setError(err.message);
+            })
 
+        fetch(`http://localhost:8088/categories`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(categoryToBeSentToServer)
+            })
+            .then(response => response.json())
+            .then(() => {
+                listCategory() // Assuming this function returns a promise
+                    .then(data => {
+                        setCategories(data);
+                    })
+                    .catch(err => {
+                        setError(err.message);
+                    });
+            })
+        console.log('Submitted label:', label)
+        setLabel('')
+        setCount(count + 1)
+    };
 
-    return (
+    return ( <>
         <div className="categories-container">
             <h1 className="title">Categories</h1>
 
-        {error && <p className="error-message">{error}</p>}
+            {error && <p className="error-message">{error}</p>}
 
             <ul className="categories-list">
                 {categories.map(category => (
@@ -34,8 +76,35 @@ export const Categories = () => {
                 ))}
             </ul>
         </div>
-    );
-}; // Empty dependency array means this effect will run once, similar to componentDidMount.
+
+        <div className="new-categories container">
+            <h1 className="title">Create a Category</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="labelInput">Enter Category Name:</label>
+                    <input
+                        type="text"
+                        id="labelInput"
+                        value={label}
+                        onChange={(e) => {
+                            setLabel(e.target.value)
+                            console.log(label)
+                        }}
+                        required
+                    />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
 
 
 
+
+
+
+
+
+
+        </div>
+    </>
+    )
+                }
